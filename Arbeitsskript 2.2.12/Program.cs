893e09f41c0f17123170bb59a2225634e6456921
Florin.Gerig <florin.gerig@icloud.com>
2025-09-01 06:59:10 +0200

    Hausaufgaben auf 1.9.25

---
+++ Arbeitsskript 2.2.12/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Arbeitsskript_2._2._12
+{
+    public class OperationenMix
+    {
+        public int leseZahl()
+        {
+            return 0;
+        }
+
+        public void printIn()
+        {
+        }
+
+        public int addiere(int a, int b)
+        {
+            return 0;
+        }
+    }
+}
